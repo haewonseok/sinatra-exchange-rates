162cc6cfdab46ce96c64daa4e2ee0f8cbff3e637
Haewon Seok <114521565+haewonseok@users.noreply.github.com>
2024-04-09 01:39:58 +0000

    Completed from_currency

--- app.rb
+++ app.rb
@@ -21,7 +21,6 @@ get("/") do
 
   # get the symbols from the JSON
   @currencies = parsed_data.fetch("currencies")
-  
   @symbols = @currencies.keys
   
   # render a view template where I show the symbols
@@ -30,14 +29,22 @@ get("/") do
 end
 
 get("/:from_currency") do
-  @original_currency = params.fetch("from_currency")
+  require "http"
   api_url = "https://api.exchangerate.host/list?access_key=#{ENV["EXCHANGE_RATE_KEY"]}"
+  raw_data = HTTP.get(api_url)
+  raw_data_string = raw_data.to_s
+
+  require "json"
+  parsed_data = JSON.parse(raw_data_string)
+  @currencies = parsed_data.fetch("currencies")
+  @symbols = @currencies.keys
+  @original_currency = params.fetch("from_currency")
   
   erb(:from_currency)
-  # some more code to parse the URL and render a view template
+
 end
 
-get("/:from_currency/:to_currency") do
+get("/:from_currency/:to_currency") do  
   @original_currency = params.fetch("from_currency")
   @destination_currency = params.fetch("to_currency")
   api_url = "https://api.exchangerate.host/convert?access_key=#{ENV["EXCHANGE_RATE_KEY"]}&from=#{@original_currency}&to=#{@destination_currency}&amount=1"
